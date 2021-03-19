c56836262af3c3c93e5f70e1608852576854f0c8
Kostya <smir.snab@gmail.com>
2021-03-19 22:36:02 +0300

    second task without  last paragr

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 " use strict";
 
-let numberOfFilms = +prompt('How many films you have watched yet?', '');
+let numberOfFilms = prompt('How many films you have watched yet?', '');
+
 // console.log(lastWatchedFilm);
 // console.log(numberOfFilms);
 // console.log(typeof(numberOfFilms));
@@ -12,24 +13,27 @@ let personalMovieDB = {
      genres : [],
      privat : false
 };
+if (personalMovieDB.count < 10) {
+     console.log('Not many films!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+     console.log('It is usual!');
+} else if (personalMovieDB.count >= 30) {
+     console.log('You are a great films lover!');
+} else {
+     console.log('It is not correct information');
+}
 
+for (let i = 0; i < 2; i++) {
 
-for (let i=1; i<3; i++) {
      let a = prompt('What was the last film you watch?', '');
-          if (a == false || a.length > 50 || a.length == 0) {
-               alert("Incorrect information! Try again");
-               i--;
-               continue;
-          }
-
      let  b = prompt('How can you score this film?', '');
-         if (b == false || b.length > 50 || b.length == 0) {
-          alert("Incorrect information! Try again");
-          i--;
-          continue;
-         } else {
+
+     if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
           personalMovieDB.movies[a] = b;
-         }
+     } else {
+          alert('It is not correct information');
+          i--;
+     }
 }
 
 
