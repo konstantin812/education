c52bcb2c390de3f55107207f343dedf749cf8ed1
Kostya <smir.snab@gmail.com>
2021-03-19 12:09:41 +0300

    first practise

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 "use strict";
 
-const category = 'toys';
+let numberOfFilms = +prompt('How many films you have watched yet?', '');
+// console.log(lastWatchedFilm);
+// console.log(numberOfFilms);
+// console.log(typeof(numberOfFilms));
 
-console.log(`https:// someurl.com/${category}/5`);
+let personalMovieDB = {
+     count : numberOfFilms,
+     movies : {},
+     actors : {},
+     genres : [],
+     privat : false
+};
 
-const user = 'Ivan';
+let lastWatchedFilm = prompt('What was the last film you watch?', '');
+let filmScore = prompt('How can you score this film?', '');
+let lastWatchedFilm1 = prompt('What was the last film you watch?', '');
+let filmScore2 = prompt('How can you score this film?', '');
 
-alert(`Hello, ${user}`);
+personalMovieDB.movies[lastWatchedFilm] = filmScore;
+personalMovieDB.movies[lastWatchedFilm1] = filmScore2;
 
-let incr = 10,
-     decr = 10 ;
 
-    //  incr++;
-    //  decr--;
 
-     console.log(++incr);
-     console.log(--decr);
+console.log(personalMovieDB);
