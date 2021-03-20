e2c4fdc7797043170b6db423b5541b1910437ff5
Kostya <smir.snab@gmail.com>
2021-03-20 12:43:37 +0300

    home task2 is ready

--- js/script.js
+++ js/script.js
@@ -23,33 +23,45 @@ if (personalMovieDB.count < 10) {
      console.log('It is not correct information');
 }
 
-// for (let i = 0; i < 2; i++) {
+for (let i = 0; i < 2; i++) {
 
-//      let a = prompt('What was the last film you watch?', '');
-//      let  b = prompt('How can you score this film?', '');
+     let a = prompt('What was the last film you watch?', '');
+     let  b = prompt('How can you score this film?', '');
 
-//      if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
-//           personalMovieDB.movies[a] = b;
-//      } else {
-//           alert('It is not correct information');
-//           i--;
-//      }
-// }
-     let i = 0;
-     while (i < 2){
-          let a = prompt('What was the last film you watch?', '');
-          let  b = prompt('How can you score this film?', '');
-
-          if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
-               personalMovieDB.movies[a] = b;
-          } else {
-               alert('It is not correct information');
-               i--;
-          }
-          i++;
+     if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+          personalMovieDB.movies[a] = b;
+     } else {
+          alert('It is not correct information');
+          i--;
      }
-     
+}
+     // let i = 0;
+     // while (i < 2){
+     //      let a = prompt('What was the last film you watch?', '');
+     //      let  b = prompt('How can you score this film?', '');
 
+     //      if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+     //           personalMovieDB.movies[a] = b;
+     //      } else {
+     //           alert('It is not correct information');
+     //           i--;
+     //      }
+     //      i++;
+     // }
+     
+     // let i = 0;
+     // do {
+     //      let a = prompt('What was the last film you watch?', '');
+     //      let  b = prompt('How can you score this film?', '');
+
+     //      if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+     //           personalMovieDB.movies[a] = b;
+     //      } else {
+     //           alert('It is not correct information');
+     //           i--;
+     //      }
+     //      i++;
+     // } while (i < 2);
      
 
 
