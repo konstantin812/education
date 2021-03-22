f1cbb101d61c8d958f57a903ff96428a8f98cc03
Kostya <smir.snab@gmail.com>
2021-03-22 15:31:34 +0300

    pracite 3

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 " use strict";
 
-let numberOfFilms = prompt('How many films you have watched yet?', '');
+let numberOfFilms; 
 
-// console.log(lastWatchedFilm);
-// console.log(numberOfFilms);
-// console.log(typeof(numberOfFilms));
+function start() {
+     numberOfFilms = +prompt('How many films you have watched yet?', ''); 
+     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+          numberOfFilms = +prompt('How many films you have watched yet?', '');
+     }
+}
 
+start();
+ 
 let personalMovieDB = {
      count : numberOfFilms,
      movies : {},
@@ -13,28 +18,56 @@ let personalMovieDB = {
      genres : [],
      privat : false
 };
-if (personalMovieDB.count < 10) {
-     console.log('Not many films!');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-     console.log('It is usual!');
-} else if (personalMovieDB.count >= 30) {
-     console.log('You are a great films lover!');
-} else {
-     console.log('It is not correct information');
-}
 
-for (let i = 0; i < 2; i++) {
+function rememberMyFilms() {
+     for (let i = 0; i < 2; i++) {
 
-     let a = prompt('What was the last film you watch?', '');
-     let  b = prompt('How can you score this film?', '');
-
-     if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
-          personalMovieDB.movies[a] = b;
-     } else {
-          alert('It is not correct information');
-          i--;
+          let a = prompt('What was the last film you watch?', '');
+          let b = prompt('How can you score this film?', '');
+     
+          if (a != null && b != null && a.length < 50 && b.length < 50 && a != '' && b != '') {
+               personalMovieDB.movies[a] = b;
+          } else {
+               alert('It is not correct information');
+               i--;
+          }
      }
 }
+
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+     if (personalMovieDB.count < 10) {
+          console.log('Not many films!');
+     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+          console.log('It is usual!');
+     } else if (personalMovieDB.count >= 30) {
+          console.log('You are a great films lover!');
+     } else {
+          console.log('It is not correct information');
+     }
+}
+
+detectPersonalLevel();
+
+
+function showMyDB() {
+     if(personalMovieDB.privat == false) {
+          console.log(personalMovieDB);
+     }
+}
+
+function writeYourGenres() {
+     for (let i = 0; i < 3; i++) {
+          let a = prompt(`What is you favoutite genres ${i+1}`, ``);
+          personalMovieDB.genres[i] = a;
+     }
+}
+writeYourGenres();
+
+console.log(personalMovieDB);
+
      // let i = 0;
      // while (i < 2){
      //      let a = prompt('What was the last film you watch?', '');
@@ -65,14 +98,7 @@ for (let i = 0; i < 2; i++) {
      
 
 
-// let a = prompt('What was the last film you watch?', '');
-// let b = prompt('How can you score this film?', '');
-// let c = prompt('What was the last film you watch?', '');
-// let d = prompt('How can you score this film?', '');
-
-
-// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
 
 
 
-console.log(personalMovieDB);
